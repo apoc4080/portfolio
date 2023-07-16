8afde4d72c616bb2ffba964ab459bde0c8d8af8d
Subodh <subodhjajee123@gmail.com>
2023-07-16 18:40:36 +0530

    final

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
           </div>
 
           <div variants={fadeIn("left", "tween", 0.2, 1)} className={css.certificate}>
-            <a target="_blank" href="https://drive.google.com/file/d/1ylGFiygJpEdTEe9Imrx7EBefcjXpFdMB/view?usp=share_link"><img src="./down.jpg" alt="" /></a>
+            <a target="_blank" href="https://drive.google.com/file/d/1bIgjyZPtb6BeK9nSm8p_ttOnfm7AkTPM/view?usp=sharing"><img src="./down.jpg" alt="" /></a>
             <a target="_blank" href="https://github.com/apoc4080"><img src="./certificate.png" alt="" /></a>
             <a target="_blank" href="https://www.linkedin.com/in/subodh-jajee-342817108/"><img src="./certificate2.png" alt="" /></a>
           </div>
